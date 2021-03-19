8d71c33ac7be25480d949c2f3e367bbeae9524dd
Stacy Read <stacy.read.007@gmail.com>
2021-03-18 23:16:18 -0500

    Initial commit

---
+++ soft-arch-tdd-cs/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace soft_arch_tdd_cs
+{
+  [TestFixture]
+  public class Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      Assert.True(true);
+    }
+  }
+}
